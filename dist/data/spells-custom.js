e73b927c69b7326460891d7ddcbab45707281243
MrDilers <75351263+MrDilers@users.noreply.github.com>
2026-06-17 23:10:25 +0200

    Mise a jour sort LRDR: Souffle de l'Humuss

--- dist/data/spells-custom.js
+++ dist/data/spells-custom.js
@@ -2,7 +2,7 @@ window.LRDR_CUSTOM_SPELLS = {
   "cra": [
     {
       "id": "souffle-de-l-humus",
-      "name": "Souffle de l'Humus",
+      "name": "Souffle de l'Humuss",
       "element": "Terre",
       "icon": "assets/icons/spells/cra/souffle-de-l-humus.png",
       "requiredLevel": "3",
