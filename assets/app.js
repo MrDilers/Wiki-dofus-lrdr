6844ecf6efacc9e76f4f1f3b6f1bddd5dee26f1d
MrDilers <mrdilerstwitch@hotmail.com>
2026-06-21 16:15:02 +0200

    Aligne la page Classes test sur mobile

--- assets/app.js
+++ assets/app.js
@@ -10,7 +10,7 @@ const state = {
   wikiFilter: "Tous",
 };
 
-const siteVersion = "1.4.1";
+const siteVersion = "1.4.2";
 
 function versionedAsset(path) {
   return `${path}${path.includes("?") ? "&" : "?"}v=${siteVersion}`;
